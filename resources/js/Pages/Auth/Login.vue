4f31aad6adbe470fee5ca9662a6a22b274ba9f28
Jan Madeyski <jan.madeyski@gmail.com>
2024-01-15 13:07:03 +0100

    login page register link

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -57,23 +57,29 @@ const submit = () => {
         <InputError class="mt-2" :message="form.errors.password" />
       </div>
 
-      <div class="block mt-4">
+      <div class="flex items-center justify-between mt-4">
         <label class="flex items-center">
           <Checkbox v-model:checked="form.remember" name="remember" />
           <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
         </label>
-      </div>
-
-      <div class="flex items-center justify-end mt-4">
         <Link v-if="canResetPassword" :href="route('password.request')"
           class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
         Forgot your password?
         </Link>
+      </div>
 
+      <div class="flex items-center justify-end mt-6">
         <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
           Log in
         </PrimaryButton>
       </div>
     </form>
+
+    <div class="mt-4 text-center text-sm text-gray-400">
+      Don't have account?
+      <Link :href="route('register')"
+        class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+      >Register</Link>
+    </div>
   </AuthenticationCard>
 </template>
